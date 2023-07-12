f60cbea783f2076fd5c8eb4eaba95de825156b06
Aleksandr Istomin <istalexnik@gmail.com>
2023-07-11 23:24:50 -0400

    squeaky solved

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,34 @@
-using System;
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < identifier.Length; i++)
+        {
+            char c = identifier[i];
+
+            if (c == '-')
+            {
+                sb.Append(identifier[++i].ToString().ToUpper());
+            }
+            else
+            {
+                sb.Append(c switch
+                {
+                    _ when c >= 'α' && c <= 'ω' => "",
+                    _ when char.IsLetter(c) => c,
+                    _ when char.IsWhiteSpace(c) => '_',
+                    _ when c == '\0' => "CTRL",
+                    _ => default
+                });
+            }
+        }
+
+        return sb.ToString();      
     }
 }
